8006320526707138ff6b0bf4c3c4f25ac8a1dd42
PulkitSharma0017 <125790209+PulkitSharma0017@users.noreply.github.com>
2023-08-15 14:55:51 +0530

    complete book create functionality

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+// import { useState } from "react";
+import BookCreate from "./components/BookCreate";
+
 function App() {
-  return <div>App</div>;
+  // const [books, setBooks] = useState([]);
+
+  const createBook = (title) => {
+    console.log("Need to add book with title: ", title);
+  };
+
+  // const editBook = () => {};
+  // const deleteBook = () => {};
+
+  return (
+    <div>
+      <BookCreate onCreate={createBook} />
+    </div>
+  );
 }
 
 export default App;
