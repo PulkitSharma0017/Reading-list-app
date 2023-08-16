462ee8d5247ea4b517a82245d4d5b7387148905c
PulkitSharma0017 <125790209+PulkitSharma0017@users.noreply.github.com>
2023-08-16 12:58:57 +0530

    display cards with book titles

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import BookCreate from "./components/BookCreate";
+import BookList from "./components/BookList";
 
 function App() {
   const [books, setBooks] = useState([]);
@@ -20,8 +21,8 @@ function App() {
   // const deleteBook = () => {};
 
   return (
-    <div>
-      {books.length}
+    <div className="app">
+      <BookList books={books} />
       <BookCreate onCreate={createBook} />
     </div>
   );
