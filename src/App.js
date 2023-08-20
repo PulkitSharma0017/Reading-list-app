924865232072e3378d06213080bc3fe51ba22b47
PulkitSharma0017 <125790209+PulkitSharma0017@users.noreply.github.com>
2023-08-20 14:26:58 +0530

    add api to delete books

--- src/App.js
+++ src/App.js
@@ -42,7 +42,9 @@ function App() {
     setBooks(updatedBook);
   };
 
-  const deleteBookById = (id) => {
+  const deleteBookById = async (id) => {
+    await axios.delete(`http://localhost:3001/books/${id}`);
+
     const updatedBooks = books.filter((book) => {
       return book.id !== id;
     });
